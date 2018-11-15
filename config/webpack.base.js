393c96b8942edf3dd181f1020e0051af21544096
susu <1039795498@qq.com>
2018-11-15 14:53:43 +0800

    feat:configuration html-webpack-pligin

--- config/webpack.base.js
+++ config/webpack.base.js
@@ -1,4 +1,5 @@
 const path = require("path")
+var HtmlwebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   // mode: 'development', // development 开发环境，production 生产环境
@@ -12,7 +13,17 @@ module.exports = {
     filename: "[name].js" // 打包后输出文件的文件名与入口文件名一致
   },
   module: {}, // 模块：栗子 解读css，图片如何转换、压缩
-  plugins: [], // 扩展插件
+  plugins: [
+    new HtmlwebpackPlugin({ // 在build目录下自动生成index.html
+      title: '', // 指定其title
+      template: './src/index.html', // 指定要打包的html路径和文件名
+      filename: 'index.html', // 指定输出路径和文件名
+      chunks: ['main1'], // 页面中所需要的js
+      minify: {
+        collapseWhitespace: true // 压缩选项
+      }
+    })
+  ], // 扩展插件
   devServer: { // 配置 webpack 开发服务功能：webpack-dev-server
     contentBase: path.resolve(__dirname, '../dist'), // 配置开发服务运行时的文件根目录
     host: 'localhost', // 服务器监听的主机地址 localhost || 127.0.0.1
--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
 
 var pNode = document.createElement("p")
 document.getElementById("root").appendChild(pNode)
+document.getElementsByTagName("p")[0].innerHTML="main1"
